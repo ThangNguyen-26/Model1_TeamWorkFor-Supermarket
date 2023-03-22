059d4b07f99cae520462bd14c3abdff87aee783d
Kim Hoang_PS24867 <>
2023-03-22 15:25:31 +0700

    Cập nhật lại MsgBox.java

--- src/main/java/com/supermarket/UTILS/MsgBox.java
+++ src/main/java/com/supermarket/UTILS/MsgBox.java
@@ -7,17 +7,17 @@ public class MsgBox {
 
     public static void alert(Component parent, String message) {
         JOptionPane.showMessageDialog(parent, message,
-                "Hệ thống quản lí đào tạo", JOptionPane.INFORMATION_MESSAGE);
+                "Hệ thống quản lí siêu thị", JOptionPane.INFORMATION_MESSAGE);
     }
 
     public static boolean confirm(Component parent, String message) {
         int result = JOptionPane.showConfirmDialog(parent, message,
-                "Hệ thống quản lý đào tạo",
+                "Hệ thống quản lý siêu thị",
                 JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
         return result == JOptionPane.YES_NO_OPTION;
     }
     public static String prompt(Component parent, String message) {
         return JOptionPane.showInputDialog(parent,message,
-                "Hệ thống quản lí đào tạo", JOptionPane.INFORMATION_MESSAGE);
+                "Hệ thống quản lí siêu thị", JOptionPane.INFORMATION_MESSAGE);
     }
 }
